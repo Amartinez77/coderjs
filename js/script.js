df9ef83be9c224fabb507c154dd96537629bfe94
Ariel Martinez <ariel.mtz85@gmail.com>
2022-05-10 20:57:44 -0300

    script que muestras las tablas de multiplicar hasta el 10 de un nro ingresado

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-let opt = parseInt(prompt("bienvenido! elige tus opciones: 1- tablas de multiplicar \n 2- salir "));
+let opt = parseInt(prompt("bienvenido! elige tus opciones: \n1- tablas de multiplicar \n2- salir \n* recuerda presionar F12 para ver los resultados "));
 
 while (opt != 2) {
-    if (opt = 1) {
-        let x = parseInt(prompt("ingrese nro: "));
-        for (let i = 0; i < 10; i++) {
-            let r = (x * i);
-            console.log(x + " por " + i + "= " + r);
+    if (opt == 1) {
 
-        }
-    } else {
-        if (opt != 1) {
-            alert("opcion incorrecta, vuelva a intentarlo");
+        let x = parseInt(prompt("ingrese un nro positivo y te mostraré su tabla hasta el 10: "));
+        if (x > 0) {
+            for (let i = 0; i <= 10; i++) {
+                let r = (x * i);
+                console.log(x + " por " + i + "= " + r);
+
+            }
+        } else {
+            alert("Recuerda que debes igresar un NÚMERO POSITIVO");
         }
 
+        console.log("gracias!");
+
+    } else if (opt != 1) {
+        alert("opcion incorrecta, vuelva a intentarlo");
     }
+
     opt = parseInt(prompt("bienvenido! elige tus opciones: 1- tablas de multiplicar \n 2- salir "));
 }
